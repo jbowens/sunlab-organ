a0d1e7ee3b4349d2361b63ffd8dddce7563aad75
Jackson Owens <jackson_owens@brown.edu>
2013-09-24 22:35:49 -0400

    started client

---
+++ client.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main(int argc, char **argv)
+{
+
+    int flags[26];
+
+    int buf;
+
+    while (read(STDIN_FILENO, (char *) &buf, 4) > 0)
+    {
+        // Update the flags
+        for (int i = 0; i < 26; i++)
+            flags[i] = (1 >> i) & buf;
+    }
+
+}
