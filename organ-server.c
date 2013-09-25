17dad4b170d94c0c1abd82e1146256e06e45746f
Jackson Owens <jackson_owens@brown.edu>
2013-09-24 21:53:44 -0400

    ssh into clients code

--- organ-server.c
+++ organ-server.c
@@ -8,18 +8,23 @@
 
 // Constants
 #define MAX_HOSTNAME_LEN    64
-static const char *MACHINES_SCRIPT = "/admin/consult/bin/lab-machines";
+#define MACHINES_SCRIPT "/admin/consult/bin/lab-machines"
+#define SSH_LOCATION "/usr/bin/ssh"
+#define CLIENT_EXECUTABLE "/home/jbowens/scratch/sunlab-organ/organ-client"
 
 // Error messages
 static const char *BAD_CHARACTER_ERROR_MSG = "Unsupported character";
 
 // Interface elements
-static const char *STR_HEADER = "The Sunlab Organ";
+static const char *STR_HEADER = "The Sunlab Organ v2 (with more bells... and more whistles)";
 static const char *STR_FLAGS_HEADER = "Currently enabled flags:";
 
 // Data type for storing information about remote children in a list.
 typedef struct remote_child {
     char hostname[MAX_HOSTNAME_LEN];
+    int pid;
+    int read_fd;
+    int write_fd;
     struct remote_child *next;
 } remote_child;
 
@@ -31,6 +36,7 @@ static void error(const char *msg);
 static void start_flag(char c);
 static void stop_flag(char c);
 static void initialize_children();
+static void launch_clients();
 
 // Currently enabled flags
 bool flags[26];
@@ -47,6 +53,9 @@ int main(int argc, char **args)
     // Get a list of sunlab machines, and initialize the data structures for
     // keeping track of them.
     initialize_children(); 
+
+    // Launch clients on all the machines
+    launch_clients();
 
     getchar();
 
@@ -214,14 +223,67 @@ static void initialize_children()
     child_head = head;
 }
 
+static void launch_clients()
+{
+    remote_child *curr_head = child_head;
+
+    while (curr_head)
+    {
+        // We can communicate with the client over ssh through a pipe! :D 
+        if (pipe(&curr_head->read_fd) == -1) {
+            perror("client pipe");
+            exit(EXIT_FAILURE);
+        }
+
+        // Fork a child process
+        int pid = fork();
+
+        if (pid < 0) {
+            perror("client fork");
+            exit(EXIT_FAILURE);
+        }
+
+        if (!pid) {
+            // This is the child process. Make this process ssh into the machine
+            // Don't need the write portion of the pipe.
+            close(curr_head->write_fd);
+            // Make the pipe be our new stdin
+            close(STDIN_FILENO);
+            dup2(curr_head->read_fd, STDIN_FILENO);
+            // SSH into the client and run the client executable
+            char * const argv[] = { SSH_LOCATION, curr_head->hostname, CLIENT_EXECUTABLE, NULL };
+            char * const envp[] = { NULL };
+            if (execve(argv[0], argv, envp) < 0) {
+                perror("ssh client");
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            // This is the parent process.
+            // Don't need the read portion of the pipe.
+            close(curr_head->read_fd);
+        }
+
+        curr_head->pid = pid;
+
+        curr_head = curr_head->next;
+    }
+}
+
+// TODO: Actually connect this sig handler
 static void finish(int signal)
 {
     endwin();
 
     // Destroy all the remote children
-    while(child_head)
+    while (child_head)
     {
         remote_child *next = child_head->next;
+
+        // Kill the pipe connection, telling the client to exit
+        close(child_head->write_fd);
+        // Wait for the client to exit cleanly
+        waitpid(child_head->pid, 0, 0);
+
         free(child_head);
         child_head = next;
     }
