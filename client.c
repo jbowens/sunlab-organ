ee6fe0209e8b142ae3ae4c11715f8644d653b993
Jackson Owens <jackson_owens@brown.edu>
2013-09-25 06:18:33 -0400

    silly /usr/bin/beep spawning client

--- client.c
+++ client.c
@@ -1,6 +1,18 @@
+#include<signal.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<unistd.h>
+
+#define BEEP_MAX_DURATION 20
+#define BEEP_EXECUTABLE "/usr/bin/beep"
+
+typedef struct a_beep {
+    int pid;
+} a_beep;
+
+a_beep *begin_beep(int freq);
+void end_beep(a_beep *beep);
 
 int main(int argc, char **argv)
 {
@@ -16,3 +28,40 @@ int main(int argc, char **argv)
     }
 
 }
+
+a_beep *begin_beep(int freq)
+{
+    a_beep *beep = malloc(sizeof(a_beep));
+    memset(beep, 0, sizeof(a_beep));
+
+    // TODO: Could avoid forking and just move writing beeps to the tty
+    // into this file.
+    
+    int pid = fork();
+
+    if (pid < 0) {
+        free(beep);
+        perror("fork");
+        return NULL;
+    }
+
+    if (!pid) {
+        // Child process, run beep!
+        char * const argv[] = { BEEP_EXECUTABLE, NULL };
+        char * const envp[] = { NULL };
+        if (execve(BEEP_EXECUTABLE, argv, envp) == -1) {
+            perror("execve");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    beep->pid = pid;
+    return beep;
+}
+
+void end_beep(a_beep *beep)
+{
+    kill(beep->pid, SIGINT);
+    free(beep);
+}
+
